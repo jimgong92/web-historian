df96a5055c8ae2e8c9332761b81c10c87161ddf0
jimgong92 <gong.jim@gmail.com>
2014-11-19 13:07:04 -0800

    added get html

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,26 @@
 var http = require("http");
+var httpRequest = require("http-request");
 var handler = require("./request-handler");
+
 
 var port = 8080;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
+var url = 'www.amazon.com';
+httpRequest.get({
+  url: url,
+  progress: function (current, total) {
+    console.log('downloaded %d bytes from %d', current, total);
+  },
+}, url, function (err,res){
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('end');
+})
+
 server.listen(port, ip);
 
