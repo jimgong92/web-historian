43c5550effbefcc4aa4ef2d315e7cd8546aa02bc
jimgong92 <gong.jim@gmail.com>
2014-11-20 16:14:33 -0800

    post working

--- web/public/app.js
+++ web/public/app.js
@@ -1,3 +1,13 @@
-$('#submit').on('click', function (){
-  var input = $('#input')
+$(document).ready(function(){
+  $('#submit').on('click', function (){
+    var input = $('#input').val();
+    $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:8080/',
+      data: { name: "John", location: "Boston" }
+    }).done(function( data ) {
+      alert(data);
+    });
+  });
 });
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -2,29 +2,50 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers');
 var fs = require('fs');
+var parseURL = require ('url');
 // require more modules/folders here!
 var urls;
 
 
 var actions = {
   "GET": function(req,res) {
-    // httpHelpers.getURL();
     var url = req.url;
     if (url === '/') {
       url += 'index.html';
     }
-
     // invoke isURLInList
     // callback that will return a boolean value
-    archive.downloadUrls('www.amazon.com');
-    console.log(archive.isURLArchived('www.amazon.com'));
-
 
     res.writeHead(200, httpHelpers.headers)
-    httpHelpers.serveAssets(res, url, function() {});
+    httpHelpers.serveAssets(res, url);
   },
   "POST": function(req,res) {
     res.writeHead(200, httpHelpers.headers)
+    req.on('data', function (chunk) {
+      // req is the website string
+      var url = chunk.toString().slice(4);
+      archive.downloadUrls(url);
+      // if isURLArchived(req)
+      if (archive.isURLArchived(url)){
+        console.log('served up cache website.');
+        // serve up cache website
+        httpHelpers.serveAssets(res, url);
+      // else check if isUrlInList(req)
+      } else if (archive.isUrlInList(url)) {
+        console.log('served up loading page.');
+        // serve up loading page
+        httpHelpers.serveAssets(res, '/loading.html');
+      // else
+      } else {
+        //addUrlToList(req)
+        archive.addUrlToList(url);
+        //downloadUrls(req)
+        archive.downloadUrls(url);
+        console.log('add to cache!');
+      }
+    });
+
+
   },
   "OPTIONS": function(req,res) {
     res.writeHead(200, httpHelpers.headers)
